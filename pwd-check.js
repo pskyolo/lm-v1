8094e8ab7728b0298a9275c8993bce1b9d1057b1
pskyolo <79154679+pskyolo@users.noreply.github.com>
2021-02-19 13:07:08 +0100

    Update pwd-check.js

--- pwd-check.js
+++ pwd-check.js
@@ -1,11 +1,3 @@
-function checkPassword(){
-    if(document.getElementById('password').value === 'LM-45erLKH!ergJK@112SQa'){
-        location.href = "welcomeWindow.html";
-    } else {
-        $(".wrong-pop").addClass("visible");
-        return false;
-    }
-}
     function closeWrong() {
         $(".wrong-pop").removeClass("visible");
     }
@@ -13,3 +5,12 @@ function checkPassword(){
    function closeWelcome() {
         $(".welcome-pop").removeClass("visible");
     }
+
+document.getElementById("key-btn").addEventListener("click", function (e) {
+        if(document.getElementById('password').value === 'LM-45erLKH!ergJK@112SQa'){
+            location.href = "welcomeWindow.html";
+        } else {
+            $(".wrong-pop").addClass("visible");
+            return false;
+        }
+});
